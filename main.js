42c85d68eb83cd42e8efa705cf6d3869c50355b3
Francisco Gómez <fgomeza25@gmail.com>
2022-07-08 13:36:16 -0600

    Mutations and more traffic

--- main.js
+++ main.js
@@ -7,17 +7,28 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 
-const N=100;
+const N=500;
 const cars = generateCars(N)
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
-    bestCar.brain = JSON.parse(
-        localStorage.getItem("bestBrain")
-    );
+    for (let i=0; i<cars.length; i++) {
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if (i!=0) {
+            NeuralNetwork.mutate(cars[i].brain, 0.3);
+        }
+    }
 }
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
 ]
 
 animate();
@@ -26,6 +37,12 @@ function save() {
     localStorage.setItem("bestBrain",
         JSON.stringify(bestCar.brain)
     );
+}
+
+function discard() {
+    if (localStorage.getItem("bestBrain")) {
+        localStorage.removeItem("bestBrain");
+    }
 }
 
 function generateCars(N) {
